0ec99c80a922daae574529a9350b8dce36130709
BarkingX <472807769@qq.com>
2024-04-06 20:28:46 +0800

    pyside test3

--- view.py
+++ view.py
@@ -1,85 +1,83 @@
+from PySide6.QtCore import QSize
+from PySide6.QtGui import QAction
 from PySide6.QtWidgets import (
     QMainWindow, QVBoxLayout, QHBoxLayout, QWidget,
-    QLabel, QPushButton, QComboBox, QLineEdit, QTextEdit, QTableWidget, QTableWidgetItem
+    QLabel, QPushButton, QComboBox, QLineEdit, QTextEdit, QTableWidget, QTableWidgetItem,
+    QSizePolicy, QSpacerItem, QStatusBar, QToolBar, QGridLayout, QTableView, QListView
 )
 
+from simulation import strings
 from simulation.model.process import Process
 
 
 class SimulationView(QMainWindow):
     def __init__(self):
         super().__init__()
+        self.setWindowTitle(strings.WINDOW_TITLE)
 
-        main_widget = QWidget()
+        main_widget = QWidget(self)
         self.setCentralWidget(main_widget)
-        main_layout = QVBoxLayout()
-        main_widget.setLayout(main_layout)
+        main_layout = QVBoxLayout(main_widget)
 
-        # 输入框架
-        input_frame = QWidget()
-        input_layout = QHBoxLayout()
-        input_frame.setLayout(input_layout)
-        main_layout.addWidget(input_frame)
-        options = [cls.__name__ for cls in Process.__subclasses__()]
-        self.dropdown = QComboBox()
-        self.dropdown.addItems(options)
-        input_layout.addWidget(self.dropdown)
-        self.burst_time_entry = QLineEdit('5')
-        input_layout.addWidget(self.burst_time_entry)
-        self.create_process_button = QPushButton('Create Process')
+        self.pause_resume_action = QAction(strings.PAUSE_RESUME, self)
+        self.pause_resume_action.setShortcut(strings.PAUSE_RESUME_KEY)
+        self.next_turn_action = QAction(strings.NEXT_TURN, self)
+        self.next_turn_action.setShortcut(strings.NEXT_TURN_KEY)
+        self.reset_action = QAction(strings.RESET, self)
+        self.reset_action.setShortcut(strings.RESET_KEY)
+
+        toolbar = QToolBar(self)
+        toolbar.setIconSize(QSize(16, 16))
+        toolbar.addAction(self.pause_resume_action)
+        toolbar.addAction(self.next_turn_action)
+        toolbar.addAction(self.reset_action)
+        self.addToolBar(toolbar)
+
+        input_widget = QWidget(main_widget)
+        # input_layout_widget = QWidget(input_widget)
+        input_layout = QHBoxLayout(input_widget)
+        main_layout.addWidget(input_widget)
+
+        self.ptype_dropdown = QComboBox(input_widget)
+        self.ptype_dropdown.addItems([cls.__name__ for cls in Process.__subclasses__()])
+        self.burst_time_input = QLineEdit(input_widget)
+        self.burst_time_input.setSizePolicy(QSizePolicy(QSizePolicy.Policy.Fixed,
+                                                        QSizePolicy.Policy.Fixed))
+        self.create_process_button = QPushButton(strings.CREATE_PROCESS, input_widget)
+        self.show_log_button = QPushButton(strings.SHOW_LOG, input_widget)
+
+        input_layout.addWidget(self.ptype_dropdown)
+        input_layout.addWidget(self.burst_time_input)
         input_layout.addWidget(self.create_process_button)
+        input_layout.addItem(QSpacerItem(40, 20, QSizePolicy.Policy.Expanding))
+        input_layout.addWidget(self.show_log_button)
 
-        # 状态显示
-        self.status_frame = QWidget()
-        status_layout = QHBoxLayout()
-        self.status_frame.setLayout(status_layout)
-        main_layout.addWidget(self.status_frame)
+        process_status_widget = QWidget(main_widget)
+        self.runnable_queue_title = QLabel(strings.RUNNABLE_QUEUE, process_status_widget)
+        self.blocked_queue_title = QLabel(strings.BLOCKED_QUEUE, process_status_widget)
+        self.process_table_view = QTableView(process_status_widget)
+        self.runnable_queue_view = QListView(process_status_widget)
+        self.blocked_queue_view = QListView(process_status_widget)
+
+        process_status_layout = QGridLayout(process_status_widget)
+        process_status_widget.setLayout(process_status_layout)
+        process_status_layout.addWidget(self.runnable_queue_title, 0, 1, 1, 1)
+        process_status_layout.addWidget(self.blocked_queue_title, 0, 2, 1, 1)
+        process_status_layout.addWidget(self.process_table_view, 1, 0, 1, 1)
+        process_status_layout.addWidget(self.runnable_queue_view, 1, 1, 1, 1)
+        process_status_layout.addWidget(self.blocked_queue_view, 1, 2, 1, 1)
+        main_layout.addWidget(process_status_widget)
+
         self.current_time_label = QLabel('Current Time: 0')
-        status_layout.addWidget(self.current_time_label)
         self.current_process_label = QLabel('Current Process ID: None')
-        status_layout.addWidget(self.current_process_label)
         self.item_count_label = QLabel('Item Count: 0')
-        status_layout.addWidget(self.item_count_label)
+        status_bar = QStatusBar(self)
+        status_bar.addPermanentWidget(self.current_time_label)
+        status_bar.addPermanentWidget(self.current_process_label)
+        status_bar.addPermanentWidget(self.item_count_label)
+        self.setStatusBar(status_bar)
 
-        # 显示区域
-        self.process_status_table = QTableWidget()
-        self.process_status_table.setColumnCount(4)
-        self.process_status_table.setHorizontalHeaderLabels(['ID', 'Type', 'State', 'Remaining Time'])
-        main_layout.addWidget(self.process_status_table)
 
-        # 列表
-        self.queue_frame = QWidget()
-        queue_layout = QHBoxLayout()
-        self.queue_frame.setLayout(queue_layout)
-        main_layout.addWidget(self.queue_frame)
-        runnable_frame = QWidget()
-        queue_layout.addWidget(runnable_frame)
-        self.runnable_label = QLabel('Runnable Processes')
-        queue_layout.addWidget(self.runnable_label)
-        self.runnable_processes_text = QTextEdit()
-        self.runnable_processes_text.setReadOnly(True)
-        queue_layout.addWidget(self.runnable_processes_text)
-        blocked_frame = QWidget()
-        queue_layout.addWidget(blocked_frame)
-        self.blocked_label = QLabel('Blocked Processes')
-        queue_layout.addWidget(self.blocked_label)
-        self.blocked_processes_text = QTextEdit()
-        self.blocked_processes_text.setReadOnly(True)
-        queue_layout.addWidget(self.blocked_processes_text)
-
-        # 控制按钮
-        self.buttons_frame = QWidget()
-        buttons_layout = QVBoxLayout()
-        self.buttons_frame.setLayout(buttons_layout)
-        main_layout.addWidget(self.buttons_frame)
-        self.show_log_button = QPushButton('Show Log')
-        buttons_layout.addWidget(self.show_log_button)
-        self.pause_resume_button = QPushButton('Pause/Resume')
-        buttons_layout.addWidget(self.pause_resume_button)
-        self.next_turn_button = QPushButton('Next Time')
-        buttons_layout.addWidget(self.next_turn_button)
-        self.reset_button = QPushButton('Reset')
-        buttons_layout.addWidget(self.reset_button)
 
     def update(self):
         pass
@@ -108,7 +106,7 @@ class SimulationView(QMainWindow):
         self.item_count_label.setText(f'Item Count: {item_count}')
 
     def process_type_and_burst_time(self):
-        return self.dropdown.currentText(), self.burst_time_entry.text()
+        return self.ptype_dropdown.currentText(), self.burst_time_input.text()
 
     def set_create_process(self, callback):
         self.create_process_button.clicked.connect(callback)
@@ -119,14 +117,14 @@ class SimulationView(QMainWindow):
         return self
 
     def set_pause_resume(self, callback):
-        self.pause_resume_button.clicked.connect(callback)
+        self.pause_resume_action.triggered.connect(callback)
         return self
 
     def set_next_turn(self, callback):
-        self.next_turn_button.clicked.connect(callback)
+        self.next_turn_action.triggered.connect(callback)
         return self
 
     def set_reset_simulation(self, callback):
-        self.reset_button.clicked.connect(callback)
+        self.reset_action.triggered.connect(callback)
         return self
 
